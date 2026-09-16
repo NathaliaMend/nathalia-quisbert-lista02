a9c339202c218ab5ba6a764d987e6e8d2438acf8
Nathalia <nathaliamend017@gmail.com>
2026-09-16 07:42:35 -0400

    exercicio resolvido e testado

---
+++ contador.js
@@ -0,0 +1,13 @@
+let i = 0;
+
+function calculaClique() {
+    i++;
+    document.getElementById("total").innerText = "Cliques: " + i;
+
+}
+
+function zeraClique() {
+    i = 0;
+    document.getElementById("total").innerText = "Cliques: " + i;
+
+}
